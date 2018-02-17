c2ccf721725699089f5131c5a81bf5d64ba218a0
Alexander Borca-Tasciuc <alexanderborcat@gmail.com>
2018-02-17 15:11:20 -0500

    First Working

--- src/org/usfirst/frc/team5149/robot/Robot.java
+++ src/org/usfirst/frc/team5149/robot/Robot.java
@@ -6,6 +6,8 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -33,8 +35,8 @@ public class Robot extends IterativeRobot {
 	String autoSelected;
 	
 	double speed = 0.25;
-	final static int LEFT_MOTOR_PORT = 0 ;
-	final static int RIGHT_MOTOR_PORT = 1;
+	final static int LEFT_MOTOR_PORT = 9 ;
+	final static int RIGHT_MOTOR_PORT = 8;
 	final static int DRIVER_JOYSTICK_PORT = 0;
 	final static int DRIVER_LEFT_AXIS = 1;
 	final static int DRIVER_RIGHT_AXIS = 5;
@@ -47,11 +49,30 @@ public class Robot extends IterativeRobot {
 	final static int DRIVER_SWITCH_MODE_BUTTON = 1;
 	final static int DRIVER_SPIN_RIGHT_POV = 90;
 	final static int DRIVER_SPIN_LEFT_POV = 270;
+	final static int ELEVATOR_PORT = 7;
+	final static int CLIMBER_PORT =6;
+	final static int RIGHT_GRABBER_PORT = 1;
+	final static int LEFT_GRABBER_PORT = 2;
+	final static int MANIPULATOR_JOYSTICK_PORT = 1;
+	private static final int ELEVATOR_AXIS = 1;
+	private static final int GRABBER_AXIS = 0;
+	private static final int SWITCH_TOP_PORT = 0;
+	private static final int SWITCH_BOTTOM_PORT = 1;
+	static boolean down = false;
+	static boolean switchMode = false;
 	
-	DigitalInput limitSwitch;
+	DigitalInput topSwitch= new DigitalInput(SWITCH_TOP_PORT);
+	DigitalInput bottomSwitch = new DigitalInput(SWITCH_BOTTOM_PORT);
 	SendableChooser<String> chooser = new SendableChooser<>();
 	RobotDrive robot = new RobotDrive(LEFT_MOTOR_PORT, RIGHT_MOTOR_PORT);
+	
+	
+	Talon elevator = new Talon(ELEVATOR_PORT);
+	Talon climber = new Talon(CLIMBER_PORT);
+	Spark rightGrabber = new Spark(RIGHT_GRABBER_PORT);
+	Spark leftGrabber = new Spark(LEFT_GRABBER_PORT);
 	Joystick driver = new Joystick(DRIVER_JOYSTICK_PORT);
+	Joystick manipulator = new Joystick(MANIPULATOR_JOYSTICK_PORT);
 	ADXRS450_Gyro gyro = new ADXRS450_Gyro(SPI.Port.kOnboardCS0);
 	
 
@@ -64,7 +85,7 @@ public class Robot extends IterativeRobot {
 		chooser.addDefault("Default Auto", defaultAuto);
 		chooser.addObject("My Auto", customAuto);
 		SmartDashboard.putData("Auto choices", chooser);
-		gyro.reset();
+		
 	}
 
 	/**
@@ -129,82 +150,9 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void teleopPeriodic() {
 		
-		boolean down = false;
-		boolean switchMode = false;  // When true, the power values will be multiplied by the slow power, reducing the total power
-		//limitSwitch = new DigitalInput(1);
-		gyro.reset();
-	
-		while (isOperatorControl() && isEnabled()) {
-			double leftPower;		// Amount of power that should go to the left motor (between 0 and 1)
-			double rightPower;		// Amount of power that should go the the right motor (between 0 and 1)
-			double speedFactor;		// Factor by which the left and right powers are multiplied by, will reduce powers if less than 1
-			boolean switchModeButton; // Keeps track of button, that when pressed, will toggle the state of switchMode 
-			double rightOffset; // The offset of the rightPower, which when greater than 0, will curve the robot right
-			double leftOffset;  // The offset of the leftPower, which when greater than 0, will curve the robot left
-			double rightMultipler;  // Amount by which the rightOffset is multiplied by, used to increase the sharpness of the curve 
-			double leftMultipler;	// Amount by which the leftOffset is multiplied by, used to increase the sharpness of the curve
-			int dpad;			// Keeps track of which dPad direction is pressed
-			double lSwitchState;	// keeps track of the limitSwitch state
-			
-			leftPower  = driver.getRawAxis(DRIVER_LEFT_AXIS);		// Get the position of the left axis 
-			rightPower = driver.getRawAxis(DRIVER_RIGHT_AXIS);		// Get the position of the right axis
-			
-			switchModeButton = driver.getRawButton(DRIVER_SWITCH_MODE_BUTTON);	// Get the current state of the switchModeButton
-			
-			rightMultipler = driver.getRawAxis(3);		// Get the intensity of the right bumper
-			leftMultipler = driver.getRawAxis(2);		// Get the intensity of the left bumper
-			
-			rightOffset = .4 * (1+rightMultipler);		
-			leftOffset = .4 * (1 + leftMultipler);
-	
-			dpad = driver.getPOV(0);				// Get the dpad direction pressed in degrees
-			
-			if (rightPower < 0)
-				rightOffset *= -1;
-			if (leftPower < 0)
-				leftOffset *= -1;
-			
-			if (switchModeButton && !down) {		// down checks if the button was already pressed
-				switchMode = !switchMode;
-				down = true;
-			} else if (!switchModeButton) {
-				down = false;
-			}
-
-			speedFactor = (switchMode) ? .5 : 1;
-
-			leftPower = leftPower * speedFactor;
-			rightPower = rightPower * speedFactor;
-			
-			if (!driver.getRawButton(DRIVER_RIGHT_TRIGGER))
-				rightOffset = 0;
-			if (!driver.getRawButton(DRIVER_LEFT_TRIGGER))
-				leftOffset = 0;
-
-			switch (dpad) {
-			case DRIVER_SPIN_RIGHT_POV:
-				robot.tankDrive(1 * speedFactor, -1 * speedFactor);
-				break;
-			case DRIVER_SPIN_LEFT_POV:
-				robot.tankDrive(-1 * speedFactor, 1 * speedFactor);
-				break;
-			case 180:
-				robot.tankDrive(rightPower - rightOffset, rightPower - leftOffset);
-				break;
-
-				
-			default:
-				
-				robot.tankDrive(rightPower - rightOffset, leftPower-leftOffset);
-			}
-			//boolean state = limitSwitch.get();
-			SmartDashboard.putNumber("Current Auto Speed" , speed);
-			//SmartDashboard.putBoolean("The switch is ", state);
-			//System.out.println("the switch is currently " + state);
-			 
-			Timer.delay(0.005);
-
-		}
+		driveOptions();
+		elevator();
+		grabber();
 
 	}
 
@@ -215,5 +163,101 @@ public class Robot extends IterativeRobot {
 	public void testPeriodic() {
 	}
 
+	public void driveOptions() {
+		// When true, the power values will be multiplied by the slow power, reducing the total power
+				
+			
+					double leftPower;		// Amount of power that should go to the left motor (between 0 and 1)
+					double rightPower;		// Amount of power that should go the the right motor (between 0 and 1)
+					double speedFactor;		// Factor by which the left and right powers are multiplied by, will reduce powers if less than 1
+					boolean switchModeButton; // Keeps track of button, that when pressed, will toggle the state of switchMode 
+					double rightOffset; // The offset of the rightPower, which when greater than 0, will curve the robot right
+					double leftOffset;  // The offset of the leftPower, which when greater than 0, will curve the robot left
+					double rightMultipler;  // Amount by which the rightOffset is multiplied by, used to increase the sharpness of the curve 
+					double leftMultipler;	// Amount by which the leftOffset is multiplied by, used to increase the sharpness of the curve
+					int dpad;			// Keeps track of which dPad direction is pressed
+					
+					
+					leftPower  = driver.getRawAxis(DRIVER_LEFT_AXIS);		// Get the position of the left axis 
+					rightPower = driver.getRawAxis(DRIVER_RIGHT_AXIS);		// Get the position of the right axis
+					
+					switchModeButton = driver.getRawButton(DRIVER_SWITCH_MODE_BUTTON);	// Get the current state of the switchModeButton
+					
+					rightMultipler = driver.getRawAxis(3);		// Get the intensity of the right bumper
+					leftMultipler = driver.getRawAxis(2);		// Get the intensity of the left bumper
+					
+					rightOffset = .4 * (1+rightMultipler);		
+					leftOffset = .4 * (1 + leftMultipler);
+			
+					dpad = driver.getPOV(0);				// Get the dpad direction pressed in degrees
+					
+					if (rightPower < 0)
+						rightOffset *= -1;
+					if (leftPower < 0)
+						leftOffset *= -1;
+					
+					if (switchModeButton && !down) {		// down checks if the button was already pressed
+						switchMode = !switchMode;
+						down = true;
+					} else if (!switchModeButton) {
+						down = false;
+					}
+
+					speedFactor = (switchMode) ? .5 : 1;
+
+					leftPower = leftPower * speedFactor;
+					rightPower = rightPower * speedFactor;
+					
+					if (!driver.getRawButton(DRIVER_RIGHT_TRIGGER))
+						rightOffset = 0;
+					if (!driver.getRawButton(DRIVER_LEFT_TRIGGER))
+						leftOffset = 0;
+
+					switch (dpad) {
+					case DRIVER_SPIN_RIGHT_POV:
+						robot.tankDrive(1 * speedFactor, -1 * speedFactor);
+						break;
+					case DRIVER_SPIN_LEFT_POV:
+						robot.tankDrive(-1 * speedFactor, 1 * speedFactor);
+						break;
+					case 180:
+						robot.tankDrive(rightPower - rightOffset, rightPower - leftOffset);
+						break;
+
+						
+					default:
+						
+						robot.tankDrive(rightPower - rightOffset, leftPower-leftOffset);
+					}
+						
+					//boolean state = limitSwitch.get();
+			
+					//SmartDashboard.putBoolean("The switch is ", state);
+					//System.out.println("the switch is currently " + state);
+					 
+		
+	}
+	
+	
+	public void elevator() {
+		double elevatorPower = manipulator.getRawAxis(ELEVATOR_AXIS);
+		boolean top = !topSwitch.get();
+		boolean bottom = !bottomSwitch.get();
+		boolean stopTop = elevatorPower < 0 && top;
+		boolean stopBottom = elevatorPower > 0 && bottom;
+		
+		if (stopTop || stopBottom) {
+			elevator.set(0);
+		}
+		else {
+			elevator.set(elevatorPower);
+		}
+		
+	}
+	public void grabber() {
+		double grabberPower = manipulator.getRawAxis(GRABBER_AXIS);
+		rightGrabber.set(grabberPower);
+		leftGrabber.set(-1 *grabberPower);
+	}
 	
 }
